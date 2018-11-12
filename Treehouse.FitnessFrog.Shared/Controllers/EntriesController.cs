eacdfcd44279983f29a78e3505d231e9cfe9c9c4
Jared Abbadusky <jrodabba@gmail.com>
2018-11-12 10:59:16 -0500

    Created Entries Controller, default route template for application

---
+++ Treehouse.FitnessFrog.Shared/Controllers/EntriesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Treehouse.FitnessFrog.Shared.Models;
+
+namespace Treehouse.FitnessFrog.Shared.Controllers
+{
+    public class EntriesController : ApiController
+    {
+        public Entry GetEntries(int id)
+        {
+            return null;
+        }
+
+        public IEnumerable<Entry> GetEntry()
+        {
+            var activityBiking = new Activity()
+            {
+                Name = "Biking"
+            };
+            return new List<Entry>()
+            {
+                new Entry(2017, 1, 2, activityBiking, 10.0m),
+                new Entry(2017, 1, 3, activityBiking, 12.2m)
+            };
+        }
+
+        public void Post()
+        {
+
+        }
+
+        public void Put()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+    }
+}
