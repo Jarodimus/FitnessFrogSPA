4226a40a06ab59e0e930d2d60c3942e9c58a6e3c
Jarodimus <jrodabba@gmail.com>
2018-11-11 23:05:31 -0500

    Added config class and passed in reference to Register method in Global.asax

--- Treehouse.FitnessFrog.Spa/Global.asax.cs
+++ Treehouse.FitnessFrog.Spa/Global.asax.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Web;
+﻿using System.Web;
+using System.Web.Http;
 
 namespace Treehouse.FitnessFrog.Spa
 {
     public class WebApiApplication : HttpApplication
     {
+        public object GlobalConfiugration { get; private set; }
+
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
