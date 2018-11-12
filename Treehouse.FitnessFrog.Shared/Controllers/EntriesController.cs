57b93561bd8a51be005ead5d954aa0efe32ca0ff
Jared Abbadusky <jrodabba@gmail.com>
2018-11-12 16:59:31 -0500

    Added SimpleInjector for dependency management

--- Treehouse.FitnessFrog.Shared/Controllers/EntriesController.cs
+++ Treehouse.FitnessFrog.Shared/Controllers/EntriesController.cs
@@ -4,43 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Treehouse.FitnessFrog.Shared.Data;
 using Treehouse.FitnessFrog.Shared.Models;
 
 namespace Treehouse.FitnessFrog.Shared.Controllers
 {
     public class EntriesController : ApiController
     {
-        public Entry GetEntries(int id)
+        private EntriesRepository _entriesRepository;
+
+        public EntriesController(EntriesRepository entriesRepository)
+        {
+            _entriesRepository = entriesRepository;
+        }
+
+        public Entry GetEntry(int id)
         {
             return null;
         }
 
-        public IEnumerable<Entry> GetEntry()
+        public IEnumerable<Entry> GetEntries()
         {
-            var activityBiking = new Activity()
-            {
-                Name = "Biking"
-            };
-            return new List<Entry>()
-            {
-                new Entry(2017, 1, 2, activityBiking, 10.0m),
-                new Entry(2017, 1, 3, activityBiking, 12.2m)
-            };
+            return _entriesRepository.GetList();
         }
 
-        public void Post()
+        public void Post(Entry entry)
         {
-
+            _entriesRepository.Add(entry);
         }
 
-        public void Put()
+        public void Put(int id, Entry entry)
         {
-
+            _entriesRepository.Update(entry);
         }
 
-        public void Delete()
+        public void Delete(int id)
         {
-
+            _entriesRepository.Delete(id);
         }
     }
 }
